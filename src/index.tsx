afc8adbf9a34991cae9b96db56a211d69b70580f
luisdourado33 <luis_dourado33@hotmail.com>
2023-01-11 09:00:41 -0400

    chore(structure): remove initials files

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +8,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <h1>My Wallet</h1>
+    </div>
   </React.StrictMode>
 );
 
