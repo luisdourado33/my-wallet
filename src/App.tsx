dc1f3e481f23e338a123a0d45e64767e377fadef
luisdourado33 <luis_dourado33@hotmail.com>
2023-01-11 16:19:42 -0400

    feat: add ChakraBaseProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import { RouterProvider } from 'react-router-dom';
-import { publicRouter } from 'navigation/public/routes';
+
 import { Provider } from 'react-redux';
 
+import { publicRouter } from 'navigation/public/routes';
 import { publicStore } from 'context/public/publicStore';
+
+import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react';
+
+import chakraTheme from '@chakra-ui/theme';
+
+const {
+  Button
+} = chakraTheme.components;
+
+const theme = extendBaseTheme({
+  components: {
+    Button
+  }
+});
 
 const App = () => {
   return (
     <Provider store={publicStore}>
-      <RouterProvider router={publicRouter} />
+      <ChakraBaseProvider theme={theme}>
+        <RouterProvider router={publicRouter} />
+      </ChakraBaseProvider>
     </Provider>
   );
 };
