b81ea0d0dcd821166410e09cc538d93817eae470
luisdourado33 <luis_dourado33@hotmail.com>
2023-01-11 21:06:22 -0400

    feat: add color palette on theme config

--- src/config/theme/theme.tsx
+++ src/config/theme/theme.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components';
 
 const theme = {
-  colors: {},
+  colors: {
+    solid: '#06283D',
+    normal: '#1363DF',
+    clean: '#47B5FF',
+    light: '#DFF6FF'
+  },
   fonts: [],
   fontSizes: {
     small: '',
