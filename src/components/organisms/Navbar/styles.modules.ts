8b37e50d88ec7780e6585ce0acd22bae4a398529
luisdourado33 <luis_dourado33@hotmail.com>
2023-01-13 15:56:00 -0400

    refactor: remove bgColor from navbar header

--- src/components/organisms/Navbar/styles.modules.ts
+++ src/components/organisms/Navbar/styles.modules.ts
@@ -5,8 +5,6 @@ import { chakra, Flex } from '@chakra-ui/react';
 export const Header = styled(chakra.header)`
   padding-inline: ${({theme}) => theme.spacing.inline};
   width: 100%;
-
-  // background-color: rgba(255,255,255,0.9);
 `;
 
 export const FlexContainer = styled(Flex)`
