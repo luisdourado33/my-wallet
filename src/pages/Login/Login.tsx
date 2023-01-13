b1062dbcc05398323bff62fa37292cf6f22a41ba
luisdourado33 <luis_dourado33@hotmail.com>
2023-01-13 16:43:26 -0400

    feat: add Login page

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './login.styles';
+
+const Login = () => {
+  return (
+    <Container>
+      <h1>Login Page</h1>
+    </Container>
+  );
+};
+
+export default Login;
